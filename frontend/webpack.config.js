b38a6281b571f8a74f56a6f7a890901f319bcb3b
adarsh50 <adarshsulegai50@gmail.com>
2022-12-17 15:57:37 +0530

    feat: working webpack setup

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -42,6 +42,7 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
+    contentBase: './public',
   },
   module: {
     rules: [
@@ -52,6 +53,17 @@ module.exports = {
       {
         test: /\.css?$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       },
       {
         test: /\.(png|j?g|svg|gif)?$/,
